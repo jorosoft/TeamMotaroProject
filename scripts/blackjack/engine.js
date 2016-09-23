70de1598bb12c4dd0be1223280f6b17d9b0c5974
Barrelrolla <chetkara@gmail.com>
2016-09-23 18:00:56 +0300

    Added a shuffle method

--- scripts/blackjack/engine.js
+++ scripts/blackjack/engine.js
@@ -22,26 +22,46 @@ var power = {
     K: 10,
 };
 
-class card {
+class Card {
     constructor(suite, power) {
         this.suite = suite;
         this.power = power;
     }
 }
 
-class deck {
+class Deck {
     constructor() {
         this.cards = [];
         for (var i = 0; i < NUMBER_OF_DECKS; i++) {
             for (var sui in suite) {
                 for (var pow in power) {
-                    var currCard = new card(sui, pow);
+                    var currCard = new Card(sui, pow);
                     this.cards.push(currCard);
                 }
             }
-        }        
+        }
+        shuffle(this.cards);
         console.log(this.cards);
     }
 }
 
-var deckOfCards = new deck();
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+var deckOfCards = new Deck();
