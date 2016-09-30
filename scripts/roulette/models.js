361364621f9a8f894b9367627e675750c2372697
ilian1902 <ilian.bratanov82@gmail.com>
2016-09-30 23:28:03 +0300

    Create in roulette/ui.js append. canvas and create class in roulette/models.js

--- scripts/roulette/models.js
+++ scripts/roulette/models.js
@@ -1,1 +1,72 @@
 // Game models
+'use strict';
+
+import * as engine from "rouletteEngine";
+
+var color = '';
+var pickNum = '';
+
+class roulette {
+
+    constructor() {
+
+    }
+
+    spin() {
+        engine.spin();
+    }
+
+    result() {
+
+        switch (text) {
+            case '3':
+            case '9':
+            case '12':
+            case '18':
+            case '21':
+            case '27':
+            case '30':
+            case '36':
+            case '5':
+            case '14':
+            case '23':
+            case '29':
+            case '32':
+            case '1':
+            case '7':
+            case '16':
+            case '19':
+            case '25':
+            case '34':
+                color = 'red';
+                pickNum = text;
+                break;
+            case '6':
+            case '15':
+            case '24':
+            case '33':
+            case '2':
+            case '8':
+            case '11':
+            case '17':
+            case '20':
+            case '26':
+            case '29':
+            case '35':
+            case '4':
+            case '10':
+            case '13':
+            case '22':
+            case '28':
+            case '31':
+                color = 'black';
+                pickNum = text;
+                break;
+            case '0':
+            case '00':
+                color = 'green';
+                break;
+        }
+
+    }
+}
