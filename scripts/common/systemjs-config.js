1c335765d137db4ac0f7c5a4772a88bc731ccd20
VenelinGP <venelin.petkov@netcoms.eu>
2016-10-03 15:59:27 +0300

    Added firebase and user-controllers in systemjs-config.js

--- scripts/common/systemjs-config.js
+++ scripts/common/systemjs-config.js
@@ -5,13 +5,12 @@ SystemJS.config({
         'systemjs-babel-build': '../lib/systemjs-babel-browser.js',
         'jquery': '../lib/jquery-3.1.0.min.js',
         'bootstrap': '../lib/bootstrap.min.js',
-        'firebase': '../lib/firebase.js',
+        'firebase': '../lib/firebase/firebase.js',
         'jqueryUI': '../lib/jquery-ui.min.js',
         'main': '../scripts/common/main.js',
         'validator': '../scripts/common/validator.js',
         'users-authentication': '../scripts/common/users-authentication.js',
-        'user-controller': '../scripts/controllers/user-controllers.js',
-
+        'user-controllers': '../scripts/controllers/user-controllers.js',
         'models': '../scripts/common/models.js',
 
         'blackjack': '../scripts/blackjack/ui.js',
